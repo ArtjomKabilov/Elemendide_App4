812cd9a0d7fa2ad38c26ce7755ebdcc502c6bc3c
opilane <opilane@HP-CZC6297BB4>
2022-04-12 12:25:43 +0300

    a

---
+++ Elemendide_App/Ajaplaan.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Elemendide_App
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Ajaplaan : ContentPage
+    {
+        public Ajaplaan()
+        {
+            InitializeComponent();
+        }
+    }
+}
